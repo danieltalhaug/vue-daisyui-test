e603ce3a0acf4277e34030a6795a7a61b6876d05
danieltalhaug <daniel.talhaug@outlook.com>
2023-10-04 14:12:27 +0200

    Setup for pinia

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+	import { useCounterStore } from './stores/counter';
 
+	const store = useCounterStore();
 </script>
 
 <template>
 	<h1>Hello world</h1>
+	<p>Count: {{ store.count }}</p>
+	<p>Double: {{ store.doubleCount }}</p>
+	<button
+		class="btn btn-primary btn-sm"
+		@click="store.increment"
+	>
+		Add
+	</button>
 </template>
 
 <style scoped>
